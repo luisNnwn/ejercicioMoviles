a431033a494144e9cc601abeaf57acf309609915
VenturaCarlos1007 <carlosven37@gmail.com>
2025-09-02 09:39:40 -0600

    WebView

--- app/src/main/java/com/example/linearlayoutwebviewymas/MainActivity.java
+++ app/src/main/java/com/example/linearlayoutwebviewymas/MainActivity.java
@@ -1,6 +1,9 @@
 package com.example.linearlayoutwebviewymas;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,15 +13,25 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
 
+    private EditText et1;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+
+        et1 = (EditText) findViewById(R.id.et1);
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+    }
+    public void ver (View v){
+        Intent i = new Intent(this, Proyecto06WV.class);
+        i.putExtra("direccion", et1.getText().toString());
+        startActivity(i);
     }
 }
